5e710a23b7e9910ecd9850a49e5e9e419e9b60d9
shmn7iii <46078198+shmn7iii@users.noreply.github.com>
2024-01-24 21:50:27 +0900

    feat: add error handling for fetch api

--- src/lib/getServerStatus.ts
+++ src/lib/getServerStatus.ts
@@ -18,17 +18,38 @@ export interface ApiResults {
   MaxPlayers: number | null;
   Port: number | null;
   Host: string | null;
-  Err: Error | null;
+  Err: Error | string | null;
 }
 
 export async function getServerStatus() {
   const url = import.meta.env.PUBLIC_KAIHEI_API_URL;
-  const response = await fetch(url, {
-    headers: {
-      "Content-Type": "application/json",
-    },
-  });
-  const data = (await response.json()) as ApiResults;
 
-  return data;
+  try {
+    const response = await fetch(url, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error(`response.status = ${response.status}, response.statusText = ${response.statusText}`);
+    }
+
+    const data = (await response.json()) as ApiResults;
+    return data;
+  } catch (err) {
+    const data: ApiResults = {
+      MOTD: null,
+      Version: null,
+      ServerVersion: null,
+      Plugins: null,
+      Map: null,
+      OnlinePlayers: null,
+      MaxPlayers: null,
+      Port: null,
+      Host: null,
+      Err: "Fetch error: " + err,
+    };
+    return data;
+  }
 }
