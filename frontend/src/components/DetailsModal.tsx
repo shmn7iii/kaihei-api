17444f357892623df980b74f2c0a8d22bdf5f6e5
shmn7iii <46078198+shmn7iii@users.noreply.github.com>
2024-02-11 21:55:11 +0900

    update: use Table class from flowbite

--- frontend/src/components/DetailsModal.tsx
+++ frontend/src/components/DetailsModal.tsx
@@ -1,4 +1,4 @@
-import { Modal, Tooltip } from "flowbite-react";
+import { Modal, Table, Tooltip } from "flowbite-react";
 import { Dispatch, SetStateAction } from "react";
 import { FaRegCopy } from "react-icons/fa";
 import { ApiResponse } from "@/hooks/fetchKaiheiStatus";
@@ -16,7 +16,7 @@ export const DetailsModal = ({
 }: DetailsModalProps): JSX.Element => {
   const mcServerHost =
     process.env.NEXT_PUBLIC_MC_SERVER_HOST ?? apiResponse?.Host ?? "dummy";
-  const mcServerAddress = mcServerHost + ":" + apiResponse?.Port;
+  const mcServerAddress = mcServerHost;
 
   const writeClipboard = (content: string) => {
     navigator.clipboard.writeText(content);
@@ -26,59 +26,59 @@ export const DetailsModal = ({
     <Modal dismissible show={openModal} onClose={() => setOpenModal(false)}>
       <Modal.Header>Details</Modal.Header>
       <Modal.Body>
-        <div className="space-y-6">
-          <table className="w-full text-left text-gray-500 rtl:text-right">
-            <tbody>
-              <tr className="bg-white">
-                <th
-                  scope="row"
-                  className="whitespace-nowrap px-6 py-4 font-medium text-gray-900"
-                >
-                  Address
-                </th>
-                <td className="px-6 py-4 text-right">
-                  <div className="flex items-center justify-end">
+        <Table className="text-base">
+          <Table.Body className="divide-y">
+            <Table.Row>
+              <Table.Cell className="text-gray-900 font-medium">
+                Address
+              </Table.Cell>
+              <Table.Cell className="text-gray-500 text-right">
+                <div className="flex items-center justify-end">
+                  <div className="mt-2">
                     <Tooltip content="Copied!" trigger="click">
                       <button onClick={() => writeClipboard(mcServerAddress)}>
                         <FaRegCopy size={17} />
                       </button>
                     </Tooltip>
-                    <p className="ms-2">{mcServerAddress}</p>
                   </div>
-                </td>
-              </tr>
-              <tr className="border-t bg-white">
-                <th
-                  scope="row"
-                  className="whitespace-nowrap px-6 py-4 font-medium text-gray-900"
-                >
-                  Version
-                </th>
-                <td className="px-6 py-4 text-right">{apiResponse?.Version}</td>
-              </tr>
-              <tr className="border-t bg-white">
-                <th
-                  scope="row"
-                  className="whitespace-nowrap px-6 py-4 font-medium text-gray-900"
-                >
-                  Players
-                </th>
-                <td className="px-6 py-4 text-right">
-                  {apiResponse?.OnlinePlayers}/{apiResponse?.MaxPlayers}
-                </td>
-              </tr>
-              <tr className="border-t bg-white">
-                <th
-                  scope="row"
-                  className="whitespace-nowrap px-6 py-4 font-medium text-gray-900"
-                >
-                  MOTD
-                </th>
-                <td className="px-6 py-4 text-right">{apiResponse?.MOTD}</td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
+                  <p className="ms-2">{mcServerAddress}</p>
+                </div>
+              </Table.Cell>
+            </Table.Row>
+            <Table.Row>
+              <Table.Cell className="text-gray-900 font-medium">
+                Version
+              </Table.Cell>
+              <Table.Cell className="text-gray-500 text-right">
+                {apiResponse?.Version}
+              </Table.Cell>
+            </Table.Row>
+            <Table.Row>
+              <Table.Cell className="text-gray-900 font-medium">
+                MOTD
+              </Table.Cell>
+              <Table.Cell className="text-gray-500 text-right">
+                {apiResponse?.MOTD}
+              </Table.Cell>
+            </Table.Row>
+            <Table.Row>
+              <Table.Cell className="text-gray-900 font-medium">
+                Players
+              </Table.Cell>
+              <Table.Cell className="text-gray-500 text-right">
+                {apiResponse?.OnlinePlayers}/{apiResponse?.MaxPlayers}
+              </Table.Cell>
+            </Table.Row>
+            <Table.Row>
+              <Table.Cell className="text-gray-900 font-medium">
+                Resolve status
+              </Table.Cell>
+              <Table.Cell className="text-gray-500 text-right">
+                REsolved!
+              </Table.Cell>
+            </Table.Row>
+          </Table.Body>
+        </Table>
       </Modal.Body>
     </Modal>
   );
